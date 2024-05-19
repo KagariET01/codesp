7dfd2f483c3cdafd0b3fb02002137736064f85f5
KagariET01 <KagariET01@users.noreply.github.com>
2024-05-19 22:12:08 +0800

    a

---
+++ pack/ZJ_c402.cpp
@@ -0,0 +1,234 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+#define INT long long int
+#define superINT INT
+#define read(n) reader<n>()
+#define PII pair<INT,INT>
+#define PPIIPII pair<PII,PII>
+#define pit(n) #n<<":"<<n
+#define MP(n,m) make_pair(n,m)
+#define endl '\n'
+#define F first
+#define S second
+#define mins(a,b) a=min(a,b)
+#define maxs(a,b) a=max(a,b)
+
+template<typename T>auto(reader)=[](){T(re);return(cin>>re,re);};
+
+//PII
+template<typename T1,typename T2>pair<T1,T2>operator+(pair<T1,T2>a,pair<T1,T2>b){
+	return pair<T1,T2>(a.F+b.F,a.S+b.S);
+}
+template<typename T1,typename T2>pair<T1,T2>operator+=(pair<T1,T2>&a,pair<T1,T2>b){
+	return a=a+b;
+}
+template<typename T1,typename T2>pair<T1,T2>operator-(pair<T1,T2>a,pair<T1,T2>b){
+	return pair<T1,T2>(a.F-b.F,a.S-b.S);
+}
+template<typename T1,typename T2>pair<T1,T2>operator-=(pair<T1,T2>&a,pair<T1,T2>b){
+	return a=a-b;
+}
+template<typename T1,typename T2,typename T3>pair<T1,T2>operator*(pair<T1,T2>a,T3 b){
+	return pair<T1,T2>(a.F*b,a.S*b);
+}
+template<typename T1,typename T2,typename T3>pair<T1,T2>operator*=(pair<T1,T2>&a,T3 b){
+	return a=a*b;
+}
+template<typename T1,typename T2,typename T3>pair<T1,T2>operator/(pair<T1,T2>a,T3 b){
+	return pair<T1,T2>(a.F/b,a.S/b);
+}
+template<typename T1,typename T2,typename T3>pair<T1,T2>operator/=(pair<T1,T2>&a,T3 b){
+	return a=a/b;
+}
+
+template<typename T1,typename T2>ostream&operator<<(ostream&ou,pair<T1,T2>p){
+	return ou<<"["<<p.first<<","<<p.second<<"]";
+}
+template<typename T1,typename T2>istream&operator>>(istream&in,pair<T1,T2>&p){
+	return in>>p.first>>p.second;
+}
+
+//deque
+template<typename T1,typename T2>ostream&operator<<(ostream&ou,deque<T1,T2>&que){
+	bool o=0;
+	ou<<"[";
+	for(T1(i):que){
+		if(o)ou<<",";
+		ou<<i;
+		o=1;
+	}
+	return(ou<<"]");
+}
+
+//stack
+template<typename T1,typename T2>ostream&operator<<(ostream&ou,stack<T1,T2>st){
+	deque<T1>dq;
+	while(!st.empty()){
+		dq.push_front(st.top());
+		st.pop();
+	}
+	return(ou<<dq);
+}
+
+//queue
+template<typename T1,typename T2>ostream&operator<<(ostream&ou,queue<T1,T2>que){
+	bool o=0;
+	ou<<"[";
+	while(!que.empty()){
+		if(o)ou<<",";
+		ou<<que.front();
+		o=1;
+		que.pop();
+	}
+	return(ou<<"]");
+}
+
+//priority_queue
+template<typename T1,typename T2,typename T3>ostream&operator<<(ostream&ou,priority_queue<T1,T2,T3>pq){
+	deque<T1>dq;
+	while(!pq.empty()){
+		dq.push_front(pq.top());
+		pq.pop();
+	}
+	return(ou<<dq);
+}
+
+//vector
+template<typename T>istream&operator>>(istream&in,vector<T>&vec){
+	for(T&i:vec)in>>i;
+	return in;
+}
+template<typename T>ostream&operator<<(ostream&ou,vector<T>vec){
+	bool o=0;
+	ou<<"[";
+	for(T(i):vec){
+		if(o)ou<<",";
+		ou<<i;
+		o=1;
+	}
+	return(ou<<"]");
+}
+
+//array
+template<typename T,std::size_t N>ostream&operator<<(ostream&ou,array<T,N>&a){
+	bool o=0;
+	ou<<"[";
+	for(T&i:a){
+		if(o)ou<<",";
+		ou<<i;
+		o=1;
+	}
+	return ou<<"]";
+}
+template<typename T,std::size_t N>istream&operator>>(istream&in,array<T,N>&a){
+	for(T&i:a)in>>i;
+	return in;
+}
+
+//list
+template<typename T,std::size_t N>istream&operator>>(istream&in,T(&a)[N]){
+	for(T&i:a)in>>i;
+	return in;
+}
+
+
+
+//map
+template<typename T1,typename T2>ostream&operator<<(ostream&ou,map<T1,T2>mp){
+	bool o=0;
+	ou<<"{";
+	for(pair<T1,T2>i:mp){
+		if(o)ou<<",";
+		ou<<i.first<<":"<<i.second;
+		o=1;
+	}
+	return(ou<<"}");
+}
+
+//io
+template<typename T>ostream&operator>>(ostream&ou,T a){
+	return ou<<a;
+}
+template<typename T>istream&operator<<(istream&in,T a){
+	return in>>a;
+}
+
+
+
+
+
+
+
+
+
+
+int main(){
+	cin.tie(0);cout.tie(0);ios::sync_with_stdio(0);
+	INT n,m;
+	cin>>n>>m;
+	vector<INT>a(n);
+	cin>>a;
+	INT asum=0;
+	for(INT&i:a)asum+=i;
+	vector<INT>mp[n];
+	while(m--){
+		INT a,b;
+		cin>>a>>b;
+		a--,b--;
+		mp[a].push_back(b);
+		mp[b].push_back(a);
+	}
+	bool wk[n]={};
+	INT flag[n]={};
+	vector<INT>b;
+	vector<INT>path;
+	bool hans=1;
+	function<bool(INT)> dfs;
+	dfs=[&](INT nw)->bool{
+		wk[nw]=1;
+		path.push_back(nw);
+		for(INT i:mp[nw]){
+			if(wk[i]){
+				if(flag[i]+flag[nw]){
+					hans=0;
+					return hans=false;
+				}
+			}else{
+				flag[i]=-flag[nw];
+				if(!dfs(i))return false;
+			}
+		}
+		return true;
+	};
+	for(INT i=0;i<n&&hans;i++){
+		if(wk[i])continue;
+		flag[i]=1;
+		dfs(i);
+		if(!hans)break;
+		INT nw=0;
+		for(INT j:path){
+			nw+=flag[j]*a[j];
+		}
+		b.push_back(abs(nw));
+		path.clear();
+	}
+	if(!hans){
+		cout<<"Bye Bye Cruel World!"<<endl;
+	}else{
+		sort(b.rbegin(),b.rend());
+		INT nw=0;
+		for(INT&i:b){
+			nw-=i;
+			nw=abs(nw);
+		}
+		INT mn=asum-nw;
+		mn/=2;
+		cout<<mn<<" "<<mn+nw<<endl;
+	}
+	return 0;
+}
+
+
+
